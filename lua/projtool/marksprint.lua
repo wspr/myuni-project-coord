0bd400e8fc5d0a5f06cbb2c1297d6b20a17894cb
Will Robertson <wspr81@gmail.com>
2021-07-14 03:08:27 +0930

    fixup and improve summarise_marks

--- lua/projtool/marksprint.lua
+++ lua/projtool/marksprint.lua
@@ -1,16 +1,45 @@
 
---local pretty = require("pl.pretty")
+local path   = require("pl.path")
+local pretty = require("pl.pretty")
+
 local proj = {}
 
-function proj:print_marks(assign_data,args)
+function proj:summarise_marks(assign_data,args)
 
+  local args = args or {}
   local who = args.who
+  local runs = args.runs or 2
+  local subpath = args.path or "assessments"
+  if not (subpath:sub(-1,-1) == "/") then
+    subpath = subpath.."/"
+  end
+  local buildpath = subpath.."build/"
   local cc = 0
+
+  if not path.exists(subpath) then
+    os.execute("mkdir "..subpath)
+  end
+  if not path.exists(subpath.."build") then
+    os.execute("mkdir "..subpath.."build")
+  end
+
+  local function texencode(str)
+    str = str:gsub("&","\\&")
+    str = str:gsub("_","\\_")
+    str = str:gsub("^","\\^")
+    str = str:gsub("%%","\\%%")
+    str = str:gsub("#","\\#")
+    str = str:gsub("{","\\{")
+    str = str:gsub("}","\\}")
+    return str
+  end
 
   for i,j in pairs(assign_data) do
     cc = cc+1
 
-    if j.user.name == who then
+    if j.user.name == (who or j.user.name) then
+
+      local filename = j.metadata.proj_id.."-"..j.user.login_id.."-rubrics"
 
       if j.provisional_grades == nil then
         error("No provisional grades? This shouldn't happen.")
@@ -19,31 +48,53 @@ function proj:print_marks(assign_data,args)
       assign_data[i].marks = assign_data[i].marks or {}
 
       print("\n"..cc..". Student: "..j.user.name)
+      print("Project ID: "..j.metadata.proj_id)
       print("Project: "..j.metadata.proj_title)
       print("Supervisor: "..j.metadata.supervisor)
       print("Moderator: "..j.metadata.moderator)
       print("URL: "..j.metadata.url)
 
-      local ff = io.output("assessments/"..j.user.login_id..".tex")
+      local ff = io.output(buildpath..filename..".tex")
       io.write [[
 \documentclass{article}
-\usepackage{booktabs}
-\usepackage[margin=2.5cm]{geometry}
+\usepackage{longtable,booktabs,needspace}
+\usepackage[margin=2.5cm,landscape]{geometry}
 \usepackage{siunitx}
 \begin{document}
-\section*{Final paper assessment ---
+\section*{%
 ]]
-      io.write(j.user.name)
+      io.write(self.assign_name_colloq .. " assessment summary")
       io.write [[
 }
 ]]
+      io.write(string.format([[
+\begin{description}
+\item[School] %s
+\item[Project ID] %s
+\item[Project title] %s
+\item[Submitting student] %s
+\item[Supervisor] %s
+\item[Moderator] %s
+\end{description}
+\newpage
+]]
+  , j.metadata.school
+  , j.metadata.proj_id
+  , texencode(j.metadata.proj_title)
+  , j.user.name
+  , j.metadata.supervisor
+  , j.metadata.moderator
+))
 
       for _,prov_grade in ipairs(j.provisional_grades) do
 
         local jd = prov_grade.rubric_assessments[#prov_grade.rubric_assessments]
         -- only take the last entry from an assessor
 
+        if jd then
+
         io.write [[
+\Needspace{0.4\textheight}
 \subsection*{Assessor ---
 ]]
         io.write(jd.assessor_name)
@@ -51,41 +102,58 @@ function proj:print_marks(assign_data,args)
 }
 ]]
         io.write [[
-\begin{tabular}{llSp{9cm}}
+\begin{longtable}{lllSp{9cm}}
 \toprule
-\# & Rubric entry & Points & Comments \\
+\# & Rubric entry & Band awarded & Points & Comments \\
 \midrule
+\endhead
 ]]
         for iid,jjd in ipairs(jd.data) do
+          local descr = jjd.description
+          if descr == "No description" then
+            descr = "---"
+          end
+          local comments = (jjd.comments or "")
+          if comments == "" then
+            comments = "[\\emph{none}]"
+          end
           io.write(
               iid.."&"..
-              jjd.description.."&"..
+              texencode(self.assignment_setup.rubric[iid].description).."&"..
+              texencode(descr).."&"..
               (jjd.points or "").."&"..
-              (jjd.comments or "")..
+              texencode(comments)..
               "\\\\\n")
         end
         io.write("\\midrule\n"..
               "".."&"..
               "\\textbf{Total}".."&"..
-              "\\textbf{"..(jd.score).."}".."&"..
+              "\\textbf{"..(jd.score or "").."}".."&"..
               ""..
               "\\\\\n")
 
         io.write [[
 \bottomrule
-\end{tabular}
+\end{longtable}
 ]]
         for _,jjd in ipairs(prov_grade.submission_comments) do
           if jd.assessor_name == jjd.author_name then
-            io.write("\\subsubsection*{Comments}")
-            io.write(jjd.comment)
+            io.write("\\subsubsection*{Comments}\\begingroup\\parskip=5pt\\parindent=0pt")
+            local comment = (jjd.comment or "")
+            io.write(texencode(comment))
+            io.write("\\subsubsection*{Comments}\\endgroup")
           end
+        end
         end
       end
       io.write [[
 \end{document}
 ]]
       io.close(ff)
+      for ii = 1,runs do
+        os.execute("cd "..buildpath.."; /Library/TeX/texbin/pdflatex "..filename.." ;")
+      end
+      os.execute("cp "..buildpath.."/"..filename..".pdf "..subpath.." ;")
     end
   end
 end
